de7a5c0458c12d2f74f9caeecef9d4973cb11c36
hamza-mujahid0 <110536624+hamza-mujahid0@users.noreply.github.com>
2023-09-06 16:03:43 +0500

    Add files via upload

---
+++ font-awesome-4.7.0/ex.js
@@ -0,0 +1,8 @@
+const pictures={
+    1: 'https://source.unsplash.com/1024x980/?bmw',
+    2: 'https://source.unsplash.com/1024x980/?lamborghini',
+    3: 'https://source.unsplash.com/1024x980/?ferarri',
+    4: 'https://source.unsplash.com/1024x980/?audi',
+    5: 'https://source.unsplash.com/1024x980/?toyota-supra'
+};
+console.log(pictures[3]);
